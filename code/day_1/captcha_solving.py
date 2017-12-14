3b0d92445600145b0fc92afb49ed2b5a80e87a3b
chgad <chgad.games@gmail.com>
2017-12-14 10:46:07 +0100

    try to if changed

--- code/day_1/captcha_solving.py
+++ code/day_1/captcha_solving.py
@@ -16,25 +16,25 @@ def varify(integer):
     return summation
 
 
-print(varify(1122))
-print(varify(1111))
-print(varify(1234))
-print(varify(91212129))
-print(varify(81112778))
+#print(varify(1122))
+#print(varify(1111))
+#print(varify(1234))
+#print(varify(91212129))
+#print(varify(81112778))
 
 
 def varify_other(integer):
     integer = list(map(int, str(integer)))
     print(integer)
     summation = 0
-    len_half = int(len(integer)/2)
+    length = len(integer)
+    len_half = int(length/2)
     for index, i in enumerate(integer):
         
-        try:
+        if index+len_half< length:
             if i == integer[index + len_half]:
                 summation += i
-        except IndexError:
-            # index + len_half is obviously greater than 
+        else:
             new_ind = index - len_half 
             if i == integer[new_ind]:
                 summation += i
@@ -42,8 +42,8 @@ def varify_other(integer):
     return summation
 
 
-#print(varify_other(1212))
-#print(varify_other(1221))
+print(varify_other(1212))
+print(varify_other(1221))
 #print(varify_other(123425))
 #print(varify_other(123123))
 #print(varify_other(12131415))
